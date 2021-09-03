aade3fa615e0ad3be0abf574c6c9f61aecd5df9c
kryptocodes <srivatsantb@gmail.com>
2021-09-03 13:11:52 +0530

    added about
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import NavBar from '../components/navbar/NavBar';
+import React from "react";
+import NavBar from "../components/navbar/NavBar";
 
-interface aboutProps {
-
-}
+interface aboutProps {}
 
 const about: React.FC<aboutProps> = ({}) => {
-        return (
-            <NavBar/>
-        );
-}
+  return (
+    <>
+      <NavBar>
+        <div>
+          <h1 className="text-4xl p-2 font-black">About</h1>
+          <hr />
+          <p className="text-justify p-4">
+            ERP Management is a feature packed. ERP Management is built with
+            Next JS,Firebase, Tailwind CSS, Headless UI . We implemented all the features you
+            might need to start a new Web application. The free version includes
+            all components, Form elements, and validation, JWT
+            authentication, Sign in, sign up pages, Vertical navigation, Lazy
+            loading, Code splitting, SASS integration.It comes with large
+            collection of React components that will offer you multiple
+            possibilities to build any kind of Apps. ERP Mangement can be used for admin
+            panel, management system, project management, CMS, CRM, Web & mobile
+            application Backend and etc.
+          </p>
+          <img src={"https://miro.medium.com/max/1400/1*Zn7mEZXZmnGu6evZiztk6g.png"} alt="about" className="w-auto p-2 rounded-lg bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500" />
+        </div>
+      </NavBar>
+    </>
+  );
+};
 
-export default about
+export default about;
